942402ce2fbe7600714f668d40d4bfaf85d2e018
Taylor Curran <taylorcurranagtc@gmail.com>
2023-06-25 13:25:19 +0200

    adding f to just_tasks

--- flows/subflow_patterns/control_just_tasks_dag.py
+++ flows/subflow_patterns/control_just_tasks_dag.py
@@ -6,8 +6,10 @@ from tasks_subflows_models.flow_params import SimulatedFailure
 from tasks_subflows_models.flow_params import SimulatedFailure
 from prefect.task_runners import ConcurrentTaskRunner
 
+# This flow contains only tasks to serve as a control against subflow behavior
+
 @task
-def child_flow_a(i, sim_failure_child_flow_a):
+def task_a(i, sim_failure_child_flow_a):
     print(f"i: {i}")
     if sim_failure_child_flow_a:
         raise Exception("This is a test exception")
@@ -16,7 +18,7 @@ def child_flow_a(i, sim_failure_child_flow_a):
 
 
 @task
-def child_flow_b(i={"i": "upstream task"}, sim_failure_child_flow_b=False):
+def task_b(i={"i": "upstream task"}, sim_failure_child_flow_b=False):
     print(f"i: {i}")
     if sim_failure_child_flow_b:
         raise Exception("This is a test exception")
@@ -25,26 +27,28 @@ def child_flow_b(i={"i": "upstream task"}, sim_failure_child_flow_b=False):
 
 
 @task
-def child_flow_d():
+def task_d():
     return {"d": "child flow d"}
 
 
 @task
-def child_flow_c():
+def task_c():
     d = "child_flow_d"
     return {"c": d}
 
-# prefect deployment build just_tasks.py:just_tasks -n dep-just-tasks -t subflows -t just-tasks -t parent -a
+# ---
+
 @flow(task_runner=ConcurrentTaskRunner(), persist_result=True)
 def just_tasks(sim_failure: SimulatedFailure = SimulatedFailure()):
     h = upstream_task_h.submit()
     i = upstream_task_i.submit()
     p = downstream_task_p.submit(h)
-    a = child_flow_a.submit(i, sim_failure.child_flow_a)
-    b = child_flow_b.submit(
+    a = task_a.submit(i, sim_failure.child_flow_a)
+    f = mid_subflow_task_f.submit()
+    b = task_b.submit(
         sim_failure_child_flow_b=sim_failure.child_flow_b, wait_for=[i]
     )
-    c = child_flow_c.submit()
+    c = task_c.submit()
     j = downstream_task_j.submit(a, c, sim_failure.downstream_task_j)
     k = downstream_task_k.submit(wait_for=[b])
 
