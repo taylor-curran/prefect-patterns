e9d8ff3c6f7c631010fd4fcb2706ff087cc54310
Taylor Curran <taylorcurranagtc@gmail.com>
2023-07-20 19:02:53 -0500

    adding qa script

---
+++ qa_temp.py
@@ -0,0 +1,17 @@
+from prefect import task, flow
+from prefect.states import Paused
+
+@task
+def pause_sometimes(pause):
+    import random
+    if random.random() > 0.5:
+        print('Pausing!!')
+        return Paused()
+
+@flow(log_prints=True) 
+def pause_flow():
+    pause_sometimes()
+    return 42
+
+if __name__ == "__main__":
+    pause_flow()
