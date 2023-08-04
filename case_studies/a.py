b411fa4d0c56970a8ce38cc6efe45e1b5773a444
Taylor Curran <taylorcurranagtc@gmail.com>
2023-08-04 15:20:37 -0500

    update

--- case_studies/a.py
+++ case_studies/a.py
@@ -7,17 +7,17 @@ from pydantic import BaseModel
 
 
 class SimulatedFailure(BaseModel):
-    task_a1: bool = False
+    task_a2: bool = False
     task_b1: bool = False
     task_t1: bool = False
 
 
 # default is 4 for sleep time
 
-# @task
-# def task_t1(sim_failure, sleep_time=2):
-#     time.sleep(sleep_time/2)
-#     return "task t1"
+@task
+def task_t1(sim_failure, sleep_time=2):
+    time.sleep(sleep_time/2)
+    return "task t1"
 
 
 # expensive task - long running - 1 day
@@ -57,43 +57,50 @@ def wrapper_task_b(sim_failure, sleep_time):
     return {"b": b.state.result()}
 
 
-# @task
-# def task_t2(b, sim_failure, sleep_time=2):
-#     print(f"I depend on {b}")
-#     return "task t2"
+@task
+def task_t2(b, sim_failure, sleep_time=2):
+    print(f"I depend on {b}")
+    return "task t2"
 
 # @task
 # def task_t3(a):
 #     print(f"I depend on {a}")
 #     return "task t3"
 
-# # first to complete - executes inside flow_a
-# @task
-# def task_a1():
-#     return "task a1"
+# first to complete - executes inside flow_a
+@task
+def task_a1():
+    return "task a1"
 
-# # expensive task - long running - 2 days - executes inside flow_a - depends on task a1
-# @task
-# def a2(a1, sim_failure, sleep_time=2):
-#     time.sleep(sleep_time)
-#     print(f"I depend on {a1}")
-#     return "task a2"
+# expensive task - long running - 2 days - executes inside flow_a - depends on task a1
+@task
+def task_a2(a1, sim_failure, sleep_time=2):
+    time.sleep(sleep_time)
 
-# @flow
-# def flow_a(t1):
-#     a1 = a1(t1)
-#     a2 = a2(a1)
+    if sim_failure.task_a2:
+        raise ValueError("simulated failure of task a2")
+    else:
+        print("task a2 finishing")
 
-# @task
-# def wrapper_task_a(t1):
-#     print("deploy run flow a")
-#     a = run_deployment(
-#         name="flow_a/a-case-a-local-docker", parameters={
-#             "sim_failure_child_flow_b": sim_failure_child_flow_b,
-#             "sleep_time": sleep_time,
-#         }
-#     )
-#     return {"a": a.state.result()}
+        return "task a2"
+
+
+@flow
+def flow_a(t1):
+    a1 = task_a1(t1)
+    a2 = task_a2(a1)
+
+@task
+def wrapper_task_a(t1):
+    print("deploy run flow a")
+    a = run_deployment(
+        name="flow_a/a-case-a-local-docker", 
+        parameters={
+            "sim_failure": sim_failure,
+            "sleep_time": sleep_time,
+        },
+    )
+    return {"a": a.state.result()}
 
 
 @flow(
@@ -104,14 +111,19 @@ def wrapper_task_b(sim_failure, sleep_time):
 def parent_flow_cs_a(sim_failure: SimulatedFailure, sleep_time: int = 4):
     if not sim_failure:
         sim_failure = SimulatedFailure()
-    b = wrapper_task_b(sim_failure, sleep_time)
-    # t1 = t1()
+    b = wrapper_task_b.submit(sim_failure, sleep_time)
+    t1 = task_t1.submit(sim_failure, sleep_time)
+    task_t2(b, sim_failure, sleep_time=2)
     # a = wrapper_task_a(t1)
     # t3 = t3(a)
 
 
 if __name__ == "__main__":
-    sim_failure = SimulatedFailure(child_a1=False, task_b1=False, task_t1=False)
+    sim_failure = SimulatedFailure(
+        task_a2=False, 
+        task_b1=False, 
+        task_t1=False
+        )
     sleep_time = 4
 
     # Call flow function
